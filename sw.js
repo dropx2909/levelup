d4047814cae2094ac35872f5db01a8472ecccd6f
dropx2909 <dropx2909@gmail.com>
2026-08-17 00:24:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
-const CACHE='level-up-static-v4';
-const FILES=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
+-const CACHE = 'level-up-static-v5';
+const FILES = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim(),
+  ]));
+});
+
+// Prefer the newest GitHub Pages files; use the copy only when offline.
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
