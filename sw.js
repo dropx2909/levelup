c9f2bc17b442e65e63b24f2947693a8a340aebd2
dropx2909 <dropx2909@gmail.com>
2026-08-17 01:29:25 +0500

    Refactor service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,24 +1,51 @@
--const CACHE = 'level-up-static-v6';
-const FILES = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
+const CACHE = 'level-up-v6';
+
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest'
+];
 
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(Promise.all([
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
-    self.clients.claim(),
-  ]));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
-// Prefer the newest GitHub Pages files; use the copy only when offline.
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).then(response => {
-    const copy = response.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return response;
-  }).catch(() => caches.match(event.request)));
+
+  event.respondWith(
+    fetch(event.request, {
+      cache: 'no-store'
+    })
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
 });
