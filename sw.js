8d9ae92b92665fe82c39580749cd3080b3a3b9a8
dropx2909 <dropx2909@gmail.com>
2026-08-17 11:16:06 +0500

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,40 +1,27 @@
-const CACHE = 'level-up-v6';
-
-const FILES = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.webmanifest'
-];
+const CACHE = 'level-up-static-v5';
+const FILES = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
-
   self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(
+      keys
+        .filter(key => key !== CACHE)
+        .map(key => caches.delete(key))
+    )),
+    self.clients.claim(),
+  ]));
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
   event.respondWith(
-    fetch(event.request, {
-      cache: 'no-store'
-    })
+    fetch(event.request)
       .then(response => {
         const copy = response.clone();
 
@@ -44,8 +31,6 @@ self.addEventListener('fetch', event => {
 
         return response;
       })
-      .catch(() => {
-        return caches.match(event.request);
-      })
+      .catch(() => caches.match(event.request))
   );
 });
