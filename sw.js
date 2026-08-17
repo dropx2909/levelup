d4f883899ab3fa19ca786f91d6f33bc0e2291630
dropx2909 <dropx2909@gmail.com>
2026-08-17 11:46:23 +0500

    Refactor cache deletion and fetch handling in sw.js

--- sw.js
+++ sw.js
@@ -8,29 +8,17 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(Promise.all([
-    caches.keys().then(keys => Promise.all(
-      keys
-        .filter(key => key !== CACHE)
-        .map(key => caches.delete(key))
-    )),
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
     self.clients.claim(),
   ]));
 });
 
+// Prefer the newest GitHub Pages files; use the copy only when offline.
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-
-        caches.open(CACHE).then(cache => {
-          cache.put(event.request, copy);
-        });
-
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
 });
